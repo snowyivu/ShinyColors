02b58ced3d8796cdcbadc85c3573aed20671f69e
snowyivu <43122270+snowyivu@users.noreply.github.com>
2019-12-15 11:04:17 -0700

    Update ShinyColors.user.js

--- ShinyColors.user.js
+++ ShinyColors.user.js
@@ -1,18 +1,18 @@
 // ==UserScript==
-// @name         偶像大师ShinyColors汉化
-// @namespace    https://github.com/biuuu/ShinyColors
+// @name         ShinyColorsENG
+// @namespace    https://github.com/snowyivu/ShinyColors
 // @version      0.9.1
-// @description  提交翻译或问题请到 https://github.com/biuuu/ShinyColors
+// @description  Submit questions or translations to https://github.com/snowyivu/ShinyColors
 // @icon         https://shinycolors.enza.fun/icon_192x192.png
-// @author       biuuu
+// @author       snowyivu
 // @match        https://shinycolors.enza.fun/*
 // @run-at       document-end
 // @grant        GM_xmlhttpRequest
 // @connect      api.interpreter.caiyunai.com
 // @connect      translate.google.cn
 // @connect      fanyi.baidu.com
-// @updateURL    https://www.shiny.fun/ShinyColors.user.js
-// @supportURL   https://github.com/biuuu/ShinyColors/issues
+// @updateURL    https://github.com/snowyivu/ShinyColors/raw/gh-pages/ShinyColors.user.js
+// @supportURL   https://github.com/snowyivu/ShinyColors/issues
 // ==/UserScript==
 (function () {
 	'use strict';
@@ -485,7 +485,7 @@
 	};
 
 	const fixDefault = data => {
-	  if (data.origin === 'https://biuuu.github.io/ShinyColors') {
+	  if (data.origin === 'https://snowyivu.github.io/ShinyColors') {
 	    data.origin = defaultConfig.origin;
 	  }
 	};
