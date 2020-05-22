422a32492d2260cb8f2aa931ca61ebb314eb4f99
biuuu <10892119+biuuu@users.noreply.github.com>
2020-05-22 19:54:42 +0800

    Update fixModule.js

--- src/utils/fixModule.js
+++ src/utils/fixModule.js
@@ -2,10 +2,14 @@ const win = window.unsafeWindow || window
 
 win.eval = new Proxy(win.eval, {
   apply(target, _this, [code]) {
-    if (code.includes('var n=window.primJsp;')) {
+    if (code.includes('var n=window.csobb3pncbpccs;')) {
       code = code.replace(
         'var n=window.primJsp;',
-        'var n=window.primJsp;window._require=t;'
+        'var n=window.csobb3pncbpccs;window._require=t;'
+      )
+      code = code.replace(
+        'Object.freeze({addHeader:',
+        '({addHeader:'
       )
     }
     return Reflect.apply(target, _this, [code])
