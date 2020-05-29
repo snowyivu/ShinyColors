2bfb41e965b4f0c3aa6aabf5de15b446db4fe8c7
snowyivu <snowyivu@gmail.com>
2020-05-29 10:03:29 -0600

    Updates

--- ShinyColors.user.js
+++ ShinyColors.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         ShinyColorsEng
 // @namespace    https://github.com/snowyivu/ShinyColors
-// @version      0.11.12
+// @version      0.11.13
 // @description  For questions or submitting translations https://github.com/snowyivu/ShinyColors
 // @icon         https://shinycolors.enza.fun/icon_192x192.png
 // @author       biuuu
 // @match        https://shinycolors.enza.fun/*
-// @run-at       document-end
+// @run-at       document-start
 // @grant        GM_xmlhttpRequest
 // @connect      api.interpreter.caiyunai.com
 // @connect      translate.google.com
@@ -483,7 +483,7 @@
 
 	var isPlainObject_1 = isPlainObject;
 
-	var version = "0.11.12";
+	var version = "0.11.13";
 
 	const PREVIEW_COUNT = 5;
 	const config = {
@@ -10201,25 +10201,73 @@
 	  document.head.appendChild(tag);
 	};
 
-	const fixModule = () => {
-	  let source = ["var n=window.csobb3pncbpccs;", "Object.freeze({addHeader:"];
-	  let result = ["var n=window.csobb3pncbpccs;window._require=t;", "({addHeader:"];
-	  const win = window.unsafeWindow || window;
-	  win.eval = new Proxy(win.eval, {
-	    apply(target, context, args) {
-	      for (let i = 0; i < source.length; i++) {
-	        if (args[0] && args[0].includes(source[i])) {
-	          args[0] = args[0].replace(source[i], result[i]);
+	// beforescriptexecute polyfill
+	if (!("onbeforescriptexecute" in document)) {
+	  // Already natively supported
+	  let scriptWatcher = new MutationObserver(mutations => {
+	    for (let mutation of mutations) {
+	      for (let node of mutation.addedNodes) {
+	        if (node.tagName === "SCRIPT") {
+	          let syntheticEvent = new CustomEvent("beforescriptexecute", {
+	            detail: node,
+	            cancelable: true
+	          }); // .dispatchEvent will execute the event synchrously,
+	          // and return false if .preventDefault() is called
+
+	          if (!document.dispatchEvent(syntheticEvent)) {
+	            node.remove();
+	          }
 	        }
 	      }
-
-	      return Reflect.apply(target, context, args);
 	    }
-
 	  });
+	  scriptWatcher.observe(document, {
+	    childList: true,
+	    subtree: true
+	  });
+	}
 
-	  win.eval.toString = () => 'function eval() { [native code] }';
+	const fixModule = text => {
+	  let source = ["var n=window.zqlboqzlz4iqbl;", "Object.freeze({addHeader:"];
+	  let result = ["var n=window.zqlboqzlz4iqbl;window._require=t;", "({addHeader:"];
+
+	  for (let i = 0; i < source.length; i++) {
+	    if (text && text.includes(source[i])) {
+	      console.log("Match found: " + source[i]);
+	      text = text.replace(source[i], result[i]);
+	    }
+	  }
+
+	  return text;
 	};
+
+	const injectModule = text => {
+	  text = fixModule(text);
+	  var newScript = document.createElement('script');
+	  newScript.type = "text/javascript";
+	  newScript.textContent = text;
+	  var head = document.getElementsByTagName('body')[0];
+	  head.appendChild(newScript);
+	};
+
+	const scriptHandler = (target, e) => {
+	  if (target.src.includes("shinycolors.enza.fun/app")) {
+	    e.preventDefault();
+	    e.stopPropagation();
+	    GM_xmlhttpRequest({
+	      method: "GET",
+	      url: target.src,
+	      onload: function (response) {
+	        injectModule(response.responseText);
+	      }
+	    });
+	  }
+	};
+
+	document.addEventListener("beforescriptexecute", function (e) {
+	  let script = e.detail || e.target;
+	  scriptHandler(script, e);
+	});
 
 	const keepBgm = () => {
 	  window.addEventListener('blur', function (e) {
@@ -10240,7 +10288,6 @@
 	  }
 	};
 
-	fixModule();
 	let waitCount = 0;
 
 	const start = async () => {
